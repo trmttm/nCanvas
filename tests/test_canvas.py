60596c484df9a4bd38bd08d9d9659f536d15110e
trmttm <96355189+trmttm@users.noreply.github.com>
2023-06-23 17:09:12 +0900

    Clean unit test undo

--- tests/test_canvas.py
+++ tests/test_canvas.py
@@ -46,64 +46,31 @@ class MyTestCase(unittest.TestCase):
         state_3 = {'shapes': ({'x': 1, 'y': 0}, {'x': 0, 'y': 0},)}
 
         canvas = Canvas()
+
+        # State 0
         self.assertEqual(canvas.state, state_0)
 
+        # State 1
         rectangle = canvas.add_rectangle()
         canvas.backup()
         self.assertEqual(canvas.state, state_1)
 
+        # State 2
         canvas.add_rectangle()
         canvas.backup()
         self.assertEqual(canvas.state, state_2)
 
+        # State 3
         rectangle.set_x(1)
         canvas.backup()
         self.assertEqual(canvas.state, state_3)
 
         canvas.show_history()
-
-        canvas.undo()
-        self.assertEqual(canvas.state, state_3)
-        canvas.undo()
-        self.assertEqual(canvas.state, state_2)
-        canvas.undo()
-        self.assertEqual(canvas.state, state_1)
-        canvas.undo()
-        self.assertEqual(canvas.state, state_0)
-
-        text_box = canvas.add_text_box()
-        text = canvas.add_text()
-        line = canvas.add_line()
-
-        rectangle.set_x(5)
-        text_box.set_x(5)
-        text.set_x(5)
-        line.set_x(5)
-        self.assertEqual(rectangle.x, 5)
-        self.assertEqual(text_box.x, 5)
-        self.assertEqual(text.x, 5)
-        self.assertEqual(line.x, 5)
-
-        rectangle.set_y(10)
-        text_box.set_y(10)
-        text.set_y(10)
-        line.set_y(10)
-        self.assertEqual(rectangle.y, 10)
-        self.assertEqual(text_box.y, 10)
-        self.assertEqual(text.y, 10)
-        self.assertEqual(line.y, 10)
-
-        rectangle.set_position(6, 11)
-        text_box.set_position(6, 11)
-        text.set_position(6, 11)
-        line.set_position(6, 11)
-        self.assertEqual(rectangle.position, (6, 11))
-        self.assertEqual(text_box.position, (6, 11))
-        self.assertEqual(text.position, (6, 11))
-        self.assertEqual(line.position, (6, 11))
-
-        # canvas.undo()
-        # self.assertEqual(canvas.state, {})
+        self.assertEqual(len(canvas.get_history()), 4)  # Check history 01
+        for state in (state_3, state_2, state_1, state_0):  # Check Undo and state
+            canvas.undo()
+            self.assertEqual(canvas.state, state)
+        self.assertEqual(len(canvas.get_history()), 0)  # Check history 02
 
 
 if __name__ == '__main__':
