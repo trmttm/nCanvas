e63ff77be7d9d9503c3948a37c30cda15c614512
trmttm <96355189+trmttm@users.noreply.github.com>
2023-06-26 09:43:52 +0900

    Define Line

--- n_canvas/shapes/line.py
+++ n_canvas/shapes/line.py
@@ -1,8 +1,15 @@
 from n_canvas import constants as c
 from .shape import Shape
 
+from .shapeproperty import ShapeProperty
+
 
 class Line(Shape):
+    width = ShapeProperty()
+    color = ShapeProperty()
+    start_arrow = ShapeProperty()
+    end_arrow = ShapeProperty()
+
     @property
     def shape_name(self) -> str:
         return c.LINE
